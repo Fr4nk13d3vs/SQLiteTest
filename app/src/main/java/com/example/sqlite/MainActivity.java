a36f824b3e589822993d2c57cabc7d8e051c81c3
DiegoFP96 <omegazero63@hotmail.com>
2023-11-09 10:02:48 -0300

    otro comentario en el codigo

--- app/src/main/java/com/example/sqlite/MainActivity.java
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         Lista = findViewById(R.id.ListaUsuarios);
         CargaUsuarios();
         //comentario en el codigo DFP
+        //otro comentario
     }
 
     public void Registrarusuario(View view){
